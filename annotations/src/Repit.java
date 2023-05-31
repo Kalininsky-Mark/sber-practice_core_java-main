509af0594052f60c1b6a8cdb4e256af5482fe38e
Kalininsky-Mark <124485970+Kalininsky-Mark@users.noreply.github.com>
2023-06-01 00:52:55 +0300

    Delete annotations/src directory

--- annotations/src/Repit.java
+++
@@ -1,9 +0,0 @@
-import java.lang.annotation.Repeatable;
-
-@Repeatable(Repits.class)
-public @interface Repit {
-    int value();
-}
-@interface Repits {
-    Repit[] value();
-}
--- annotations/src/tests.java
+++
@@ -1,7 +0,0 @@
-public class tests {
-    public static void main(String[] args){
-        DataContainer container = new DataContainer();
-        container = null;
-        test3.printStr(container);
-    }
-}
